c60fb8f55fb110801c729b7bb9b984eb12dfc7ba
Sean Yang <seanyang0813@gmail.com>
2020-12-28 00:42:46 -0500

    server

---
+++ server.js
@@ -0,0 +1,13 @@
+const io = require("socket.io")(3000);
+
+const users = {}
+
+io.on('connection', socket => {
+  socket.on('join-room', function(room) {
+    socket.join(room);
+  });
+  socket.on('message', function(messageObject) {
+      socket.to(messageObject.room).emit("message-send", 
+        {user: messageObject.user, message: messageObject.message})
+  });
+})
