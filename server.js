f3330eb69ac464d3c1be244cde17b97d5a47182d
Sean Yang <seanyang0813@gmail.com>
2020-12-29 21:34:58 -0500

    log

--- server.js
+++ server.js
@@ -1,17 +1,24 @@
 const io = require("socket.io")(3000, {
     cors: {
-      origin: "*",
+      origin: "http://backofzoom.vercel.app/",
     },
   });
 
-const users = {}
+var connections = 0;
+var messages = 0;
 
 io.on('connection', socket => {
   socket.on('join-room', function(room) {
     socket.join(room);
+    connections += 1;
+    if (connections % 20 === 0) {
+      console.log("connections: " + connections.toString());
+      console.log("Messages: " + messages.toString());
+    }
   });
   socket.on('message', function(messageObject) {
-      socket.to(messageObject.room).emit("message-send", 
-        {user: messageObject.user, message: messageObject.message})
+    messages += 1;
+    socket.to(messageObject.room).emit("message-send", 
+      {user: messageObject.user, message: messageObject.message})
   });
 })
